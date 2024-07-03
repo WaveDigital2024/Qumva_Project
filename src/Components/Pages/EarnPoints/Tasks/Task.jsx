a4584ab5a12296add5e5c36c1bc4cd8dde7a0907
unknown <borshonsaha1234@gmail.com>
2024-07-03 03:19:39 -0700

    task delete system update

--- src/Components/Pages/EarnPoints/Tasks/Task.jsx
+++ src/Components/Pages/EarnPoints/Tasks/Task.jsx
@@ -1,39 +1,60 @@
 
-import axios from 'axios';
 import PropTypes from 'prop-types';
 import { IoMdArrowDropright } from 'react-icons/io';
 import useTaskComplete from '../../../../Hooks/useTaskComplete';
 import { AuthContext } from '../../../../Providers/Authproviders';
 import { useContext } from 'react';
+import useUserInfo from '../../../../Hooks/useUserInfo';
+import useAxiosSecure from '../../../../Hooks/useAxiosSecure';
+import { ToastContainer, toast } from 'react-toastify';
 
 
 const Task = ({ singletask, index }) => {
     const [, refetch] = useTaskComplete()
     const { user } = useContext(AuthContext);
+    const [userinfo] = useUserInfo()
+    const axiosSecure = useAxiosSecure()
     const { title, points, twitter_link, done, _id } = singletask
     const handlecompletask = async () => {
-        axios.patch(`http://localhost:5000/completetask/${_id}`)
-        .then(res => {
-            if (res.data.acknowledged) {
-                refetch();
-                axios.patch(`http://localhost:5000/updateuserpoints`, { email: user.email, points })
-                    .then(userRes => {
-                        if (userRes.data.acknowledged) {
-                            console.log('User points updated successfully');
-                        } else {
-                            console.error('Failed to update user points');
-                        }
-                    })
-                    .catch(error => {
-                        console.error('Error updating user points:', error);
-                    });
-            }
-        })
+        axiosSecure.patch(`/completetask/${_id}`)
+            .then(res => {
+                if (res.data.acknowledged) {
+                    refetch();
+                    axiosSecure.patch(`/updateuserpoints`, { email: user.email, points })
+                        .then(userRes => {
+                            if (userRes.data.acknowledged) {
+                                console.log('User points updated successfully');
+                            } else {
+                                console.error('Failed to update user points');
+                            }
+                        })
+                        .catch(error => {
+                            console.error('Error updating user points:', error);
+                        });
+                }
+            })
             .catch(error => {
                 console.error('Error updating task:', error);
             });
 
     };
+
+    // admin delete
+    const handleTaskDelete = () => {
+        console.log('admin delete');
+        axiosSecure.delete(`/deletetask/${_id}`)
+            .then(res => {
+                if (res.data.deletedCount > 0) {
+                    toast('done');
+                    refetch();
+                } else {
+                    toast('No items deleted');
+                }
+            })
+            .catch(error => {
+                toast('Error:', error);
+            });
+    }
     return (
         <div className="flex justify-between items-center px-4 w-11/12 mx-auto bg-gradient-to-l from-[#010103e1]  via-[#20002c] to-[#07000c] py-3 mt-4 rounded-xl ">
             {/* task details */}
@@ -59,6 +80,12 @@ const Task = ({ singletask, index }) => {
                     Completed
                 </button>
             )}
+            {
+                userinfo[0]?.userRole === 'admin' && <div className="card-actions justify-end">
+                    <button onClick={handleTaskDelete} className="px-5 py-3 bg-gradient-to-l from-[#321c4e] via-[#1a0161] to-[#170247] rounded-xl font-extrabold uppercase">Delete</button>
+                </div>
+            }
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
