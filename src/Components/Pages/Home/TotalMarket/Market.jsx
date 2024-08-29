48f126eb8fc64a0fb331a743e6237d0e993760a7
unknown <borshonsaha1234@gmail.com>
2024-08-29 08:01:12 -0700

    market updated

--- src/Components/Pages/Home/TotalMarket/Market.jsx
+++ src/Components/Pages/Home/TotalMarket/Market.jsx
@@ -11,23 +11,20 @@ const Market = () => {
         <div className="px-4 max-w-6xl mx-auto text-white font-poppins ">
             <Title title={'QUMVA TOKENOMICS'}></Title>
             <h1 className="text-2xl md:text-3xl px-4 uppercase font-extrabold text-center">Total Supply: 350,000,000,000</h1>
-            <h1>Qumva Network</h1>
-            <div>
-                <h1>Symbol : QMV</h1>
-                <h1>Network : Ton</h1>
-            </div>
+
+           
             <div className="flex flex-col lg:flex-row gap-4 px-4 mt-3 mb-6 justify-between items-center">
                 {/* content */}
-                
+
                 <div>
-                    
+
                     {/* cap */}
                     <div>
                         {/* 1 */}
                         <div className="flex flex-row gap-2 items-center mt-4 mb-4 px-4">
                             <h1 className="text-xl md:text-2xl px-2 text-[#000000]"><BsBoxFill /></h1>
                             <div>
-                                <h1  className="text-lg md:text-xl font-bold ">LIQUIDITY</h1>
+                                <h1 className="text-lg md:text-xl font-bold ">LIQUIDITY</h1>
                                 <p className="text-sm md:text-lg text-slate-200">LP (10%) + CEX Liquidity (7%)</p>
                             </div>
                         </div>
@@ -35,7 +32,7 @@ const Market = () => {
                         <div className="flex flex-row gap-2 items-center mt-4 mb-4 px-4">
                             <h1 className="text-xl md:text-2xl px-2 text-[#2d8ea4]"><BsBoxFill /></h1>
                             <div>
-                                <h1  className="text-lg md:text-xl font-bold ">TEAM</h1>
+                                <h1 className="text-lg md:text-xl font-bold ">TEAM</h1>
                                 <p className="text-sm md:text-lg text-slate-200">TGE 10%, linear vesting 24 months</p>
                             </div>
                         </div>
@@ -43,7 +40,7 @@ const Market = () => {
                         <div className="flex flex-row gap-2 items-center mt-4 mb-4 px-4">
                             <h1 className="text-xl md:text-2xl px-2 text-[#ffab91]"><BsBoxFill /></h1>
                             <div>
-                                <h1  className="text-lg md:text-xl font-bold ">ECOSYSTEM FUND</h1>
+                                <h1 className="text-lg md:text-xl font-bold ">ECOSYSTEM FUND</h1>
                                 <p className="text-sm md:text-lg text-slate-200">TGE 20%, linear vesting 12 months</p>
                             </div>
                         </div>
@@ -51,7 +48,7 @@ const Market = () => {
                         <div className="flex flex-row gap-2 items-center mt-4 mb-4 px-4">
                             <h1 className="text-xl md:text-2xl px-2 text-[#ffccbc]"><BsBoxFill /></h1>
                             <div>
-                                <h1  className="text-lg md:text-xl font-bold ">PUBLIC SALE</h1>
+                                <h1 className="text-lg md:text-xl font-bold ">PUBLIC SALE</h1>
                                 <p className="text-sm md:text-lg text-slate-200">TGE 100%, Full unlock</p>
                             </div>
                         </div>
@@ -59,7 +56,7 @@ const Market = () => {
                         <div className="flex flex-row gap-2 items-center mt-4 mb-4 px-4">
                             <h1 className="text-xl md:text-2xl px-2 text-[#f9f3e3]"><BsBoxFill /></h1>
                             <div>
-                                <h1  className="text-lg md:text-xl font-bold ">RESERVE </h1>
+                                <h1 className="text-lg md:text-xl font-bold ">RESERVE </h1>
                                 <p className="text-sm md:text-lg text-slate-200">Treasury for future needs & expansion</p>
                             </div>
                         </div>
@@ -67,7 +64,7 @@ const Market = () => {
                         <div className="flex flex-row gap-2 items-center mt-4 mb-4 px-4">
                             <h1 className="text-xl md:text-2xl px-2 text-[#a0a5c2]"><BsBoxFill /></h1>
                             <div>
-                                <h1  className="text-lg md:text-xl font-bold ">AIRDROP & BOUNTY PROGRAM </h1>
+                                <h1 className="text-lg md:text-xl font-bold ">AIRDROP & BOUNTY PROGRAM </h1>
                                 <p className="text-sm md:text-lg text-slate-200">TGE 100%, Full unlock</p>
                             </div>
                         </div>
@@ -76,10 +73,16 @@ const Market = () => {
                 </div>
                 {/* img */}
                 <div className=" w-full lg:w-2/3 flex flex-col justify-center items-center">
-                    <img src={marketimg} alt="marketcap" />
-                   
+                    
+                    <a href="https://tonviewer.com/EQBUqHtbPkj-wW2IBZFdhxs_C11LpE4xb8Lc5UY64N2bJ0xI" target="_blank"><img src={marketimg} alt="marketcap" /></a>
 
                 </div>
+            </div>
+            <div className="flex flex-row gap-2 max-w-4xl mx-auto rounded-2xl justify-center items-centerborder-2 bg-gradient-to-r from-slate-900/50 to-sky-950/70 border-sky-300 ">
+
+                <h1 className="text-xl font-bold text-white text-center">Qumva Network  |</h1>
+                <h1 className="text-lg font-bold  text-center">       SYMBOL : QMV </h1>
+                <h1 className="text-lg font-bold  text-center">|       NETWORK : Ton </h1>
             </div>
 
         </div>
